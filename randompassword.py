2c1f67c2b9a84db4ee73f09e63e66c87ac3982da
shourov-sorkar <18103142@iubat.edu>
2021-02-17 02:33:42 +0600

    :closed_lock_with_key: Random Password generator

---
+++ randompassword.py
@@ -0,0 +1,15 @@
+import string
+import random
+ 
+print("+++Itsourcecode Password Generator+++")
+ 
+mxchars = string.ascii_letters+string.digits+string.punctuation
+ 
+numInput = int(input("Enter the Number of passwords to Generate: "))
+length = int(input("Enter the Password Length: "))
+print("Generated Password:  ")
+for pwd in range(numInput):
+    pw = ''
+    for c in range(length):
+            pw += random.choice(mxchars)
+print(pw)
